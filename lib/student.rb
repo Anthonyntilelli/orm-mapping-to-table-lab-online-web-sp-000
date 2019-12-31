4dc29d0715b7d88e9f0a3cf5a7cc618b55be9710
Anthony Tilelli <anthony@tilelli.me>
2019-12-31 06:29:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,8 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initiliaze 
+  
+  def initialize(name,grade,id=nil)
+  end
   
 end
