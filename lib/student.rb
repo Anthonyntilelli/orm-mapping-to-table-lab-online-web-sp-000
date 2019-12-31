2f94b3d48a840e627d2d724cb81e6e46c2ae3c35
Anthony Tilelli <anthony@tilelli.me>
2019-12-31 06:39:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,7 @@ class Student
   
   def save
     sql = <<-SQL
-    INSERT INTO students (name,grade) 
-    VALUES(?,?)
+    INSERT INTO students (name,grade) VALUES(?,?)
     SQL
     DB[:conn].execute(sql,@name,@grade)
     @id = DB[:conn].execute(sql,@name,@grade)
